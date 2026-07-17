4929de64464962ae71cc3437409182eb6bc1f68b
uroosa-ak <uroosaab.karim549@gmail.com>
2026-07-17 13:01:49 +0500

    package install for session

--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
+//import//
 const express = require('express');
 const dotenv=require('dotenv');
 const ConnectDB = require('./config/db');
 dotenv.config();
+const mongoose = require("mongoose");
+const session = require("express-session");
 const app = express()
 ConnectDB();
-
-
-
-
+//Middelwear//
+app.use(express.json());
+app.use(express.urlencoded({extented: false}));
+app.set("view engine","ejs");
+app.use(session({
+    secret: "hotel-secret",
+    resave: false,
+    saveUninitialized: false
+}));
+secret: process.env.SESSION_SECRET
+//routes//
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
