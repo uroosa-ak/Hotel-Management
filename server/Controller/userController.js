f652b74574d5747654ac31a7799b326bb5e61b5e
Uroosa <uroosaab.karim549@gmail.com>
2026-07-20 22:24:22 -0700

    password-work

--- server/Controller/userController.js
+++ server/Controller/userController.js
@@ -1,41 +1,89 @@
-const User =    require("../models/User")
+const User = require("../models/User")
+const bcrypt = require(bcrypt)
 let userController = {
-    register: async (req,res)=>{
-        console.log(req.body)
-        let {username,email,password, role,contact,age} = req.body
+  register: async (req, res) => {
+    console.log(req.body)
+    let { username, email, password, role, contact, age } = req.body
     try {
-        if (!username|| !email|| !password) {
+      if (!username || !email || !password) {
+        return res.json({
+          message: "Required fields are missing",
+          status: false
+        })
+      }
+      else {
+        let existingUser = await user.findone({ email })
+        console.log(existingUser)
+        if (existingUser) {
           return res.json({
-            message:"Required fields are missing",
-            status:false   
-          }) }
-          else{
-            let newUser=  await User.create({username,email,password, role,contact,age})
-         return res.json({
-            message:"Account created",
-            status:true,
-            newUser   
-          })   
-        } 
-    } catch (error) {
-        res.json({
-            message: error.messaage,
+            message: "Account already exist with this email ",
             status: false
-        })
-        
+          })
+        }
+        else {
+          let hashPass = await bcrypt.hash(password, 10)
+          console.log(hashPass)
+          let newUser = await User.create({ username, email, password, role, contact, age })
+
+          return res.json({
+            message: "Account created",
+            status: true,
+            newUser
+          })
+
+        }
+      }
+
+
+    } catch (error) {
+      res.json({
+        message: error.messaage,
+        status: false
+      })
+
     }
-},
-    login: (req,res)=>{
+  },
+  login: async (req, res) => {
+    let {eamil,password}= req.body
     try {
-        
-    } catch (error) {
-        res.json({
-            message: error.messaage,
-            status: false
-        })
-        
+if (!email || !password) {
+    return res.json({
+        message: "Required fiels missng",
+        status: false
+      })
+  
+} else {
+  let existingUser= await user.findone({email})
+  if (!existingUser) {
+     return res.json({
+        message: "No account exist",
+        status: false
+      })
+  }
+   else {
+    let isPassMatch = await  bcrypt.compare(password,existingUser.password)
+    if (isPassMatch) {
+        return res.json({
+        message: "Login Successful",
+        status: true,
+        user: existingUser
+      })
+    } else {
+       res.json({
+        message: "invalid password",
+        status: false
+      })
     }
+  }
 }
+    } catch (error) {
+      res.json({
+        message: error.messaage,
+        status: false
+      })
+
+    }
+  }
 }
 ////chat gpt code//
 const User = require("../models/User");
